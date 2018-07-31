47b49ab693e6a57717fe9081288445e60ec9c66d
dj-kostya <nosorev_ka@gkl-kemerovo.ru>
2018-07-31 14:25:11 +0700

    Add files via upload

---
+++ 31072018/BuildAll.sql
@@ -0,0 +1,6 @@
+@C:\Users\user\Desktop\31072018\table.sql;
+@C:\Users\user\Desktop\31072018\type.sql;
+@C:\Users\user\Desktop\31072018\procedure.sql;
+@C:\Users\user\Desktop\31072018\trigger.sql;
+@C:\Users\user\Desktop\31072018\sequence.sql;
+@C:\Users\user\Desktop\31072018\function.sql;
